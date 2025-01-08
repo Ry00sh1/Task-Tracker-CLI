a3d3c54bc7564aab8a9c5284ea9080335c33d20c
Pedro Sena <phasena@gmail.com>
2025-01-08 14:27:46 +0000

    feat: delete and update tasks

--- ToDo/main.py
+++ ToDo/main.py
@@ -5,20 +5,28 @@ from tabulate import tabulate
 
 TASKS_FILE =  os.path.join(os.path.dirname(__file__),'src', 'tasks.json')
 
+def search_task_by_id(file_path, task_id):
+    tasks = read_file(file_path)
+
+    # Search for the task with the given ID
+    for task in tasks[1:]:
+        if task.get('id') == task_id:
+            return task
+    return print("Task not found. Verify if the ID exists or it's correct")
+
 def read_and_update_last_id(file_path):
     # Read the JSON file
-    loaded_json = read_file(file_path)
+    tasks = read_file(file_path)
 
     # Access the lastID
-    last_id = loaded_json[0]['lastID']
+    last_id = tasks[0]['lastID']
 
     # Update the lastID
     new_last_id = last_id + 1
-    loaded_json[0]['lastID'] = new_last_id
+    tasks[0]['lastID'] = new_last_id
 
     # Write the updated JSON content back to the file
-    with open(file_path, 'w') as file:
-        json.dump(loaded_json, file, indent=4)
+    write_task(tasks, file_path)
 
     return new_last_id
 
@@ -57,26 +65,60 @@ def display_tasks(file_path):
     # Display data in a tabular format
     print(tabulate(rows, headers=headers, tablefmt="simple"))
 
-def update_task():
-    pass
-def delete_task():
-    pass 
+def update_task(file_path, id):
+    # Read the JSON file
+    tasks = read_file(file_path)
+
+    # Find the task by id
+    for task in tasks[1:]:
+        if task.get('id') == id:
+            print("Update...\n1. Description\n2. Status")
+            choice = int(input("Enter your choice: "))
+            if choice == 1:
+                input("Type the new description: ")
+                new_description = input()
+                updatedAt = str(datetime.now())
+                task.update({'description': new_description, 'updatedAt': updatedAt})
+                
+            elif choice == 2:
+                print("What's the new status? (todo, in-progress, done):")
+                new_status = input()
+                updatedAt = str(datetime.now())
+                task.update({'status': new_status})
+        else: 
+            print("Task not found. Verify if the ID exists or if it's correct.")
+            return 
+
+ 
+
+    # Write the updated JSON content back to the file
+    write_task(tasks, file_path)
+    
+    print("Task updated successfully.")
+    
+
+def delete_task(file_path, id):
+    tasks = read_file(file_path)
+
+    tasks = [task for task in tasks if task.get('id') != task_id]
+
+    write_task(tasks, file_path)
 
 if __name__ == "__main__":
     while True:
         print("\nCRUD Operations:")
         print("1. Create task")
         print("2. Display tasks")
-        print("3. Update Record")
-        print("4. Delete Record")
+        print("3. Update task")
+        print("4. Delete task")
         print("5. Exit")
         
         choice = input("Enter your choice: ")
 
         if choice == '1':
             id = read_and_update_last_id(TASKS_FILE)
-            description = input("Descreva a tarefa: ")
-            status = input("Qual o status da tarefa? (Finalizada, Em Progresso ou Concluída): ")
+            description = input("Describe the task: ")
+            status = input("What is the status of the task? (todo, in-progress, done): ")
             createdAt = str(datetime.now())
             updatedAt = str(datetime.now())
 
@@ -86,14 +128,12 @@ if __name__ == "__main__":
             display_tasks(TASKS_FILE)
 
         elif choice == '3':
-            record_id = int(input("Enter ID to update: "))
-            name = input("Enter new name: ")
-            age = int(input("Enter new age: "))
-            update_task(record_id, {"name": name, "age": age})
+            task_id = int(input("Enter task ID to update: "))
+            update_task(TASKS_FILE,task_id)
 
         elif choice == '4':
-            record_id = int(input("Enter ID to delete: "))
-            delete_task(record_id)
+            task_id = int(input("Enter task ID to delete: "))
+            delete_task(TASKS_FILE, task_id)
 
         elif choice == '5':
             break
